6011d166a949042d495a96ba2cb80e4eea24d7ba
Ethan Carnahan <ethan.carnahan@gmail.com>
2021-09-03 15:20:06 -0700

    Add project files.

---
+++ BadCutType/HarmonyPatches/Patches.cs
@@ -0,0 +1,54 @@
+﻿using HarmonyLib;
+using IPA.Utilities;
+using TMPro;
+using UnityEngine;
+using Zenject;
+
+namespace BadCutType.HarmonyPatches
+{
+    [HarmonyPatch(typeof(BadNoteCutEffectSpawner), nameof(BadNoteCutEffectSpawner.HandleNoteWasCut), MethodType.Normal)]
+    internal class BadCutTextSpawner_HandleNoteWasMissed
+    {
+        public static bool inMethod = false;
+        static FlyingTextSpawner _spawner;
+        static FlyingTextSpawner _spawnerBase
+        {
+            get
+            {
+                if (_spawner != null) return _spawner;
+                else
+                {
+                    _spawner = new GameObject("BadCutTextSpawner").AddComponent<FlyingTextSpawner>();
+
+                    var installers = Object.FindObjectsOfType<MonoInstallerBase>();
+                    foreach (var installer in installers)
+                    {
+                        var container = installer.GetProperty<DiContainer, MonoInstallerBase>("Container");
+
+                        if (container != null && container.HasBinding<FlyingSpriteEffect.Pool>())
+                        {
+                            container.Inject(_spawner);
+                            break;
+                        }
+                    }
+                    _spawner.SetField("_fontSize", 2f);
+                    _spawner.SetField("_color", Color.red);
+
+                    return _spawner;
+                }
+            }
+        }
+
+        [HarmonyPrefix]
+        static bool Prefix(BadNoteCutEffectSpawner __instance, NoteController noteController, in NoteCutInfo noteCutInfo)
+        {
+            if (_spawnerBase == null) {
+                Plugin.Log.Error("Failed to inject FlyingTextSpawner!");
+                return true;
+            }
+            if (!noteCutInfo.failReason.Equals(NoteCutInfo.FailReason.None))
+                _spawner.SpawnText(noteCutInfo.cutPoint, noteController.worldRotation, noteController.inverseWorldRotation, Plugin.FailText(noteCutInfo.failReason));
+            return false;
+        }
+    }
+}
---
+++ BadCutType/Plugin.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using IPA;
+using IPA.Config;
+using IPA.Config.Stores;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+using HarmonyLib;
+using IPALogger = IPA.Logging.Logger;
+
+namespace BadCutType
+{
+
+    [Plugin(RuntimeOptions.SingleStartInit)]
+    public class Plugin
+    {
+        internal static Plugin Instance { get; private set; }
+        internal static IPALogger Log { get; private set; }
+        private Harmony harmony;
+
+        [Init]
+        /// <summary>
+        /// Called when the plugin is first loaded by IPA (either when the game starts or when the plugin is enabled if it starts disabled).
+        /// [Init] methods that use a Constructor or called before regular methods like InitWithConfig.
+        /// Only use [Init] with one Constructor.
+        /// </summary>
+        public void Init(IPALogger logger)
+        {
+            Instance = this;
+            Log = logger;
+            Log.Info("BadCutType initialized.");
+        }
+
+        #region BSIPA Config
+        //Uncomment to use BSIPA's config
+        /*
+        [Init]
+        public void InitWithConfig(Config conf)
+        {
+            Configuration.PluginConfig.Instance = conf.Generated<Configuration.PluginConfig>();
+            Log.Debug("Config loaded");
+        }
+        */
+        #endregion
+
+        [OnStart]
+        public void OnApplicationStart()
+        {
+            Log.Debug("OnApplicationStart");
+            harmony = new Harmony("DreamReality.BadCutType");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+
+        [OnExit]
+        public void OnApplicationQuit()
+        {
+            harmony.UnpatchAll(harmony.Id);
+            Log.Debug("OnApplicationQuit");
+        }
+
+        public static string FailText(NoteCutInfo.FailReason reason) {
+            switch (reason) {
+                case NoteCutInfo.FailReason.WrongDirection: return "Direction";
+                case NoteCutInfo.FailReason.WrongColor: return "Color";
+                case NoteCutInfo.FailReason.TooSoon: return "Timing";
+                case NoteCutInfo.FailReason.CutHarder: return "Swing";
+                default: return "None";
+            }
+        }
+    }
+}
